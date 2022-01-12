e714eab19a91157ce27f2d07c03769abadb06a25
gh-yu <yooumi99@gmail.com>
2022-01-12 20:24:16 +0900

    관리자-회의실 목록 조회/등록/수정 기능 구현

--- CODEUS/src/main/java/com/codeusgroup/codeus/admin/model/service/MeetRoomService.java
+++ CODEUS/src/main/java/com/codeusgroup/codeus/admin/model/service/MeetRoomService.java
@@ -1,5 +1,17 @@
 package com.codeusgroup.codeus.admin.model.service;
 
+import java.util.List;
+
+import com.codeusgroup.codeus.meetingResrv.model.vo.MeetingRoom;
+
 public interface MeetRoomService {
+
+	List<MeetingRoom> selectMeetRoomList();
+
+	int insertMeetRoom(MeetingRoom meetRoom);
+
+	MeetingRoom selectMeetRoom(int meet_no);
+
+	int updateMeetRoom(MeetingRoom meetRoom);
 
 }
--- CODEUS/src/main/java/com/codeusgroup/codeus/admin/model/service/MeetRoomServiceImpl.java
+++ CODEUS/src/main/java/com/codeusgroup/codeus/admin/model/service/MeetRoomServiceImpl.java
@@ -1,10 +1,13 @@
 package com.codeusgroup.codeus.admin.model.service;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.codeusgroup.codeus.admin.model.dao.MeetRoomDAO;
+import com.codeusgroup.codeus.meetingResrv.model.vo.MeetingRoom;
 
 @Service("meetService")
 public class MeetRoomServiceImpl implements MeetRoomService {
@@ -14,6 +17,26 @@ public class MeetRoomServiceImpl implements MeetRoomService {
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	@Override
+	public List<MeetingRoom> selectMeetRoomList() {
+		return meetDAO.selectMeetRoomList(sqlSession);
+	}
+
+	@Override
+	public int insertMeetRoom(MeetingRoom meetRoom) {
+		return meetDAO.insertMeetRoom(sqlSession, meetRoom);
+	}
+
+	@Override
+	public MeetingRoom selectMeetRoom(int meet_no) {
+		return meetDAO.selectMeetRoom(sqlSession, meet_no);
+	}
+
+	@Override
+	public int updateMeetRoom(MeetingRoom meetRoom) {
+		return meetDAO.updateMeetRoom(sqlSession, meetRoom);
+	}
 	
 	
 }
